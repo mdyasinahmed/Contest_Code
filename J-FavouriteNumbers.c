a9adcb0cfca44328e815777858c102194ef90be8
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-25 07:40:43 +0600

    problem favourite number is solved

--- J-FavouriteNumbers.c
+++ J-FavouriteNumbers.c
@@ -1,1 +1,26 @@
-#include
+#include<stdio.h>
+
+int main()
+{
+    int t, a;
+    scanf("%d", &t);
+
+    while(t--)
+    {
+        scanf("%d", &a);
+
+        if(a%2==0 && a%7==0)
+        {
+            printf("Alice\n");
+        }
+        else if(a%2!=0 && a%9==0)
+        {
+            printf("Bob\n");
+        }
+        else{
+            printf("Charlie\n");
+        }
+    }
+
+    return 0;
+}
