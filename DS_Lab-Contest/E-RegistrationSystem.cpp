9baf5e16ca112a4e0f058a5884bfb70ae9c9071e
mdyasinahmed <yasinahmed7875@gmail.com>
2023-05-27 08:40:16 +0600

    problem E - Registration System solution code is added

--- DS_Lab-Contest/E-RegistrationSystem.cpp
+++ DS_Lab-Contest/E-RegistrationSystem.cpp
@@ -1,29 +1,31 @@
-#include <cstdio>
+#include <iostream>
 #include <string>
 #include <map>
-
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
+    int t;
+    cin>>t;
 
     map<string, int> database;
-    string s;
-    while (n--)
+    
+    string str;
+
+    while(t--)
     {
-        cin >> s;
-        if (database.count(s) == 0)
+        cin>>str;
+        if(database.count(str) == 0)
         {
-            cout << "OK" << endl;
-            database[s] = 1;
+            cout<<"OK"<<endl;
+            database[str] = 1;
         }
         else
         {
-            cout << s << database[s] << endl;
-            database[s] += 1;
+            cout<<str<<database[str]<<endl;
+            database[str] += 1;
         }
     }
+
     return 0;
 }
