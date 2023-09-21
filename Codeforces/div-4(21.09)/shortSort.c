96e078947b8c299f8990dd7014b2bce089979edc
mdyasinahmed <yasinahmed7875@gmail.com>
2023-09-21 21:47:14 +0600

    add shortSort solved code

---
+++ Codeforces/div-4(21.09)/shortSort.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int t;
+    char s;
+    scanf("%d", &t);
+
+    while(t--) {
+        
+        scanf("%s", &s);
+    }
+
+    for(int i=0; i<s;i++){
+
+    }
+    printf("%s", s);
+}
