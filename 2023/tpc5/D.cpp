d64d28fcf86577e248e64ecba32f9594d1e4fe67
mdyasinahmed <yasinahmed7875@gmail.com>
2023-10-12 13:18:22 +0600

    add solved problem code

--- 2023/tpc5/D.cpp
+++ 2023/tpc5/D.cpp
@@ -10,15 +10,12 @@ int main() {
         cin >> s;
 
         stack<char> strStack; 
-        for(int i=0; i< strStack.size(); i++) {
-            cout <<"Top: " << strStack.top() << endl;
-            // if(!strStack.empty()) {
-            //     if() {
-            //         strStack.pop(); 
-            //     }
-            // } else {
-            //     strStack.push(c);
-            // }
+        for(int i=0; i< strStack.size();) {
+            if(!strStack.empty() && strStack.top() == 'A' && c == 'B') {
+                strStack.pop(); 
+            } else {
+                strStack.push(c);
+            }
         }
 
         cout << strStack.size() << endl;
---
+++ 2023/tpc5/testE.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string a, b;
+    cin >> a;
+    cin >> b;
+
+    if(a.size() < b.size())
+        swap(a, b);
+
+    int j = a.size()-1;
+    for(int i=b.size()-1; i>=0; i--, j--)
+        a[j]+=(b[i]-'0');
+
+    for(int i=a.size()-1; i>0; i--) {
+        if(a[i] > '9') {
+            int d = a[i]-'0';
+            a[i-1] = ((a[i-1]-'0') + d/10) + '0';
+            a[i] = (d%10)+'0';
+        }
+    }
+    if(a[0] > '9') {
+        string k;
+        k+=a[0];
+        a[0] = ((a[0]-'0')%10)+'0';
+        k[0] = ((k[0]-'0')/10)+'0';
+        a = k+a;
+    }
+    
+    
+    cout << a << endl;
+}
